beb7f87de54ab2089902c8c0253cb93bd4d39b31
sgasimov-dotcom <sahib.gasimov2@gmail.com>
2021-09-08 20:17:12 -0500

    we

---
+++ cron.daily.2am/cron_project.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+# in ubuntu /var/run/reboot-required is needed for reboot to fully update the packages
+# insert this to /etc/crontab *   *  *  *  * ~/bash_course/cron_script.sh
+
+sudo apt -y update
+sudo apt -y upgrade
+
+if [ -f /var/run/reboot-required ]; then
+        sudo reboot
+fi
